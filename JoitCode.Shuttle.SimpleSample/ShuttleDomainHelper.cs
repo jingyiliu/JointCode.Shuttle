5df602b1d211ca334af68d0298866d0d93962ac5
jingyiliu <jingeelio@gmail.com>
2017-07-25 15:29:34 +0800

    add MIT license

--- JoitCode.Shuttle.SimpleSample/ShuttleDomainHelper.cs
+++ JoitCode.Shuttle.SimpleSample/ShuttleDomainHelper.cs
@@ -1,48 +1,48 @@
-using System;
-using System.Reflection;
-using System.Reflection.Emit;
-using JointCode.Common.Extensions;
-using JointCode.Expressions;
-using JointCode.ServiceInjector;
-using JointCode.Shuttle;
+//using System;
+//using System.Reflection;
+//using System.Reflection.Emit;
+//using JointCode.Common.Extensions;
+//using JointCode.Expressions;
+//using JointCode.ServiceInjector;
+//using JointCode.Shuttle;
 
-namespace JoitCode.Shuttle.SimpleSample
-{
-    public static class ShuttleDomainHelper
-    {
-        public static ShuttleDomain Create(string assemblySymbol, string assemblyName)
-        {
-            return Create(assemblySymbol, assemblyName, null);
-        }
+//namespace JoitCode.Shuttle.SimpleSample
+//{
+//    public static class ShuttleDomainHelper
+//    {
+//        public static ShuttleDomain Create(string assemblySymbol, string assemblyName)
+//        {
+//            return Create(assemblySymbol, assemblyName, null);
+//        }
 
-        public static ShuttleDomain Create(string assemblySymbol, string assemblyName, ServiceContainer svContainer)
-        {
-            var dynAsmOptions = new DynamicAssemblyOptions
-            {
-                AccessMode = AssemblyBuilderAccess.Run,
-                AssemblyName = new AssemblyName(assemblyName)
-            };
+//        public static ShuttleDomain Create(string assemblySymbol, string assemblyName, ServiceContainer svContainer)
+//        {
+//            var dynAsmOptions = new DynamicAssemblyOptions
+//            {
+//                AccessMode = AssemblyBuilderAccess.Run,
+//                AssemblyName = new AssemblyName(assemblyName)
+//            };
 
-            var options = new ShuttleDomainOptions
-            {
-                DynamicAssemblySymbol = assemblySymbol,
-                DynamicAssemblyOptions = dynAsmOptions,
-                DefaultLeaseTime = 10.Seconds(),
-                PollingInterval = 5.Seconds()
-            };
+//            var options = new ShuttleDomainOptions
+//            {
+//                DynamicAssemblySymbol = assemblySymbol,
+//                DynamicAssemblyOptions = dynAsmOptions,
+//                DefaultLeaseTime = 10.Seconds(),
+//                PollingInterval = 5.Seconds()
+//            };
 
-            try
-            {
-                return ShuttleDomain.Create(ref options, svContainer);
-            }
-            catch(Exception e)
-            {
-                if (e.InnerException != null)
-                    Console.WriteLine(e.InnerException.Message);
-                else
-                    Console.WriteLine(e.Message);
-                return null;
-            }
-        }
-    }
-}
+//            try
+//            {
+//                return ShuttleDomain.Create(ref options, svContainer);
+//            }
+//            catch(Exception e)
+//            {
+//                if (e.InnerException != null)
+//                    Console.WriteLine(e.InnerException.Message);
+//                else
+//                    Console.WriteLine(e.Message);
+//                return null;
+//            }
+//        }
+//    }
+//}
